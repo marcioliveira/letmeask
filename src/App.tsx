93c88d8473757cfc66a219ff032e0b0ef68b4f30
marcioliveira <marcioruelaoliveira@gmail.com>
2021-06-22 13:11:24 -0300

    explicação dos conceitos de componente e propriedade do react

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { Button } from './components/Button';
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Button>Clique aqui</Button>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </div>
   );
 }
 
