8b39ea39eb9420dcff0051b62a37a699df0ba35b
marcioliveira <marcioruelaoliveira@gmail.com>
2021-06-28 11:47:31 -0300

    terminado a estrutura das perguntas html e css

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -4,12 +4,13 @@ import { useParams } from 'react-router-dom';
 import logoImg from '../assets/images/logo.svg';
 
 import { useAuth } from '../hooks/useAuth';
+import { Question } from '../components/Question';
 import { Button } from "../components/Button";
 import { RoomCode } from '../components/RoomCode';
 import { database } from '../services/firebase';
 
 import '../styles/room.scss';
-import { title } from 'process';
+//import { title } from 'process';
 
 type FirebaseQuestions = Record<string, {
     author: {
@@ -22,7 +23,7 @@ type FirebaseQuestions = Record<string, {
     isHighlighted: boolean;
 }>
 
-type Question = {
+type QuestionType = {
     id: string;
     author: {
         name: string;
@@ -42,7 +43,7 @@ export function Room() {
     const { user } = useAuth();
     const params = useParams<RoomParams>();
     const [newQuestion, setNewQuestion] = useState('');
-    const [questions, setQuestions] = useState<Question[]>([]);
+    const [questions, setQuestions] = useState<QuestionType[]>([]);
     const [ title, setTitle ] = useState('');
 
     const roomId = params.id;
@@ -129,7 +130,17 @@ export function Room() {
                         <Button type="submit" disabled={!user}>Enviar pergunta</Button>
                     </div>
                 </form>   
-                {JSON.stringify(questions)}             
+                <div className="question-list">
+                    {questions.map(question => {
+                        return (
+                            <Question
+                                key={question.id} 
+                                content={question.content}
+                                author={question.author}
+                            />
+                        )
+                    })}
+                </div>
             </main>
         </div>
     )
